7f9643f16855c823aa34bb5c00cc38438d5f95c4
abdellAtmani <40188133+abdellAtmani@users.noreply.github.com>
2019-03-11 14:40:44 +0100

    test1

--- src/test/java/JenkinsDemo.java
+++ src/test/java/JenkinsDemo.java
@@ -1,53 +1,38 @@
+
+import static org.junit.Assert.*;
+
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Created by burakergoren
- * Sample junit test code to integrate by
- */
-public class JenkinsDemo
-{
-    private static String Base_Url = "https://www.facebook.com";
-    private WebDriver driver;
+public class JenkinsDemo {
+	//protected UtilsMath mat;
+	
+	@Before
+	public void setUp() throws Exception {
+		//mat =new UtilsMath();
+		System.out.println("before");
+	}
 
-    @Before
-    public void setUp()
-    {
-        driver = new ChromeDriver();
-        driver.get(Base_Url);
-    }
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("reussi");
+	}
 
-    @After
-    public void after()
-    {
-        driver.quit();
-    }
+	@Test
+	public void testAddition() throws Exception {
+		//assertEquals(new Long(4), mat.addition(new Long(1), new Long(3)));
+		assertEquals(4, 4);
+	}
+	
+	@Test
+	public void testAdditionInt1() throws Exception {
+		assertEquals(4, 3);
+	}
+	@Test
+	public void testAdditionInt2() throws Exception {
+		//assertEquals(3,mat.additionInt(1, 4));
+		assertEquals(4, 4);
+	}
 
-    @Test
-    public void testCasePassed()
-    {
-        //Assert.assertTrue(driver.findElement(By.xpath("//form[@id='login_form']")).isDisplayed());
-        //assertEquals(4, 4);
-    }
-
-    @Test
-    public void testCaseFailed()
-    {
-        //Assert.assertTrue(driver.findElement(By.xpath("//form[@id='failed case']")).isDisplayed());
-         //assertEquals(4, 4);
-    }
-
-    @Ignore
-    @Test
-    public void testCaseIgnored()
-    {
-        //Assert.assertTrue(driver.findElement(By.xpath("//form[@id='ignored case']")).isDisplayed());
-        //assertEquals(4, 4);
-    }
 }
